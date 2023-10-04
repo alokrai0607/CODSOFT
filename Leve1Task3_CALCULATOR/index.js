8844508d4b48854c6af5b3293e150913eb76db48
alokrai0607 <saelalok001@gmail.com>
2023-10-04 12:06:31 +0530

    Calculator App

---
+++ Leve1Task3_CALCULATOR/index.js
@@ -0,0 +1,22 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    try {
+        const result = eval(displayValue);
+        document.getElementById('display').value = result;
+        displayValue = result.toString();
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+        displayValue = '';
+    }
+}
